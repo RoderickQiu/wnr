9ab24ba7b16bcb98cb2be18ed22e2da0274a3a06
RoderickQiu <scrisqiu@hotmail.com>
2019-01-22 15:53:44 +0800

    make main frame

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # wnr
+
+The name is a abbr of "Work & Rest". It's a timer app with strong expansibility for computers.
+
+Copyright (c) 2019 RoderickQiu
---
+++ main.js
@@ -0,0 +1,67 @@
+const { app, BrowserWindow, ipcMain, Tray, Menu, globalShortcut } = require('electron')
+const Store = require('electron-store');
+const store = new Store();//设置数据读取
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+let tray = null
+
+function createWindow() {
+    // 创建浏览器窗口。
+    win = new BrowserWindow({ width: 324, height: 233, frame: false, resizable: true, show: false, hasShadow: true, webPreferences: { nodeIntegration: true } })
+
+    // 然后加载应用的 index.html。
+    win.loadFile('index.html')
+
+    //在加载页面时，渲染进程第一次完成绘制时，会发出 ready-to-show 事件。在此事件后显示窗口将没有视觉闪烁
+    win.once('ready-to-show', () => {
+        win.show()
+        //win.webContents.openDevTools()
+    })
+
+    // 当 window 被关闭，这个事件会被触发。
+    win.on('closed', () => {
+        // 取消引用 window 对象，如果你的应用支持多窗口的话，
+        // 通常会把多个 window 对象存放在一个数组里面，
+        // 与此同时，你应该删除相应的元素。
+        win = null
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
+    })
+}
+
+// 当程序就要结束
+app.on('will-quit', () => {
+    // 清空所有快捷键
+    globalShortcut.unregisterAll()
+})
+
+
+
+// Electron 会在初始化后并准备
+// 创建浏览器窗口时，调用这个函数。
+// 部分 API 在 ready 事件触发后才能使用。
+app.on('ready', () => {
+    createWindow()
+
+    tray = new Tray('./res/icons/iconWin.ico')
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Show/Hide', click: () => { win.isVisible() ? win.hide() : win.show() } },
+        { label: 'Exit', click: () => { app.quit() } }
+    ])
+    tray.setToolTip('wnr')
+    tray.setContextMenu(contextMenu)
+    tray.on('click', () => {
+        win.isVisible() ? win.hide() : win.show()
+    })//托盘菜单
+})
+
+app.on('activate', () => {
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
+    if (win === null) {
+        createWindow()
+    }
+})
