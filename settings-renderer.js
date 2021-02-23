f5bbdead174a00c7bcefe38e4b243bff53b910ca
r-q <scrisqiu@hotmail.com>
2021-02-23 22:53:05 +0800

    optimize reserved tasks

--- settings-renderer.js
+++ settings-renderer.js
@@ -390,6 +390,7 @@ let reservedArray = store.has("reserved") ? store.get("reserved") : [];
 let newReservedItem = {
     id: store.get("reserved-record") + 1,
     time: "23:59",
+    endTime: "24:00",
     plan: "0",
     cycle: "0"
 };
@@ -468,6 +469,18 @@ function reservedEdit(index) {
     reservedArray[index].time = $("#reserved-time-" + index).val();
     reservedArray[index].plan = $("#dropdown-itemlist-" + index).attr("value");
     reservedArray[index].cycle = $("#reserved-cycle-" + index).val();
+
+    let planInfo = store.get("predefined-tasks")[reservedArray[index].plan];
+    let planTotalTime = (planInfo.workTime + planInfo.restTime) * planInfo.loops;
+    let endTimeHourPart = Number(reservedArray[index].time.slice(0, 2)) + Math.floor(planTotalTime / 60);
+    let endTimeMinutePart = Number(reservedArray[index].time.slice(3, 5)) + planTotalTime % 60;
+    if (endTimeMinutePart >= 60) endTimeMinutePart -= 60, endTimeHourPart += 1;
+    if (endTimeHourPart >= 24) endTimeHourPart = 24, endTimeMinutePart = 0;
+    endTimeHourPart = endTimeHourPart.toString(), endTimeMinutePart = endTimeMinutePart.toString();
+    if (endTimeHourPart.length < 2) endTimeHourPart = "0" + endTimeHourPart;
+    if (endTimeMinutePart.length < 2) endTimeMinutePart = "0" + endTimeMinutePart;
+    reservedArray[index].endTime = endTimeHourPart + ":" + endTimeMinutePart;
+
     store.set("reserved", reservedArray);
 }
 
