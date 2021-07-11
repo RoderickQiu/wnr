RoderickQiu/wnr

commit 5507a2d92e4d0729569f71d9e0d17fe4150e7ec7

separate alert settings for work/rest
also corrected pausing

main.js
@@ -437,6 +437,12 @@ app.on('ready', () => {
 
     if (!store.has("disable-pausing-special")) store.set("disable-pausing-special", "all");
 
+    if (store.has("no-check-time-end")) {
+        store.set("no-check-work-time-end", store.get("no-check-time-end"));
+        store.set("no-check-rest-time-end", store.get("no-check-time-end"));
+        store.delete("no-check-time-end");
+    }
+
     store.set("just-launched", true);
 
     if (process.platform === "darwin") {
@@ -1267,7 +1273,7 @@ function nonFocusSolution() {
     }
 }
 
-function timeEndDialogDispose(mode) {
+function noCheckTimeSolution(mode) {
     if ((mode === "rest" ? workTimeFocused : restTimeFocused) && (!isLoose)) {
         fullScreenProtection = true;
     } else {
@@ -1277,21 +1283,12 @@ function timeEndDialogDispose(mode) {
         }
         if (dockHide) app.dock.hide();
     }
-    win.webContents.send('warning-closed');
     win.maximizable = false;
 }
 
-function noCheckTimeSolution() {
-    if (restTimeFocused && (!isLoose)) {
-        fullScreenProtection = true;
-    } else {
-        if (store.get("top") !== true) {
-            win.setAlwaysOnTop(false);//cancel unnecessary always-on-top
-            if (!hasFloating) win.moveTop();
-        }
-        if (dockHide) app.dock.hide();
-    }
-    win.maximizable = false;
+function timeEndDialogDispose(mode) {
+    noCheckTimeSolution(mode);
+    win.webContents.send('warning-closed');
 }
 
 ipcMain.on('warning-giver-workend', function () {
@@ -1312,9 +1309,11 @@ ipcMain.on('warning-giver-workend', function () {
                 (store.has("personalization-notification.work-time-end-msg") ?
                     store.get("personalization-notification.work-time-end-msg") : i18n.__('work-time-end-msg')), "normal");
         }
-        if (store.get("no-check-time-end")) {
-            noCheckTimeSolution();
-        } else
+
+        if (store.get("no-check-work-time-end")) {
+            noCheckTimeSolution("work");
+            setTimeout(() => win.webContents.send("alter-start-stop", "start"), 1000);
+        } else {
             setTimeout(function () {
                 if (process.platform !== "darwin" || (process.platform === "darwin" && restTimeFocused))
                     dialog.showMessageBox(win, {
@@ -1327,6 +1326,7 @@ ipcMain.on('warning-giver-workend', function () {
                             + " " + (hasMultiDisplays ? "\r" + i18n.__('has-multi-displays') : ""),
                     }).then(function () {
                         timeEndDialogDispose("work");
+                        //win.webContents.send("alter-start-stop", "start");
                     });
                 else dialog.showMessageBox({
                     title: " wnr",
@@ -1338,9 +1338,11 @@ ipcMain.on('warning-giver-workend', function () {
                         + " " + (hasMultiDisplays ? "\r" + i18n.__('has-multi-displays') : ""),
                 }).then(function () {
                     timeEndDialogDispose("work");
+                    //win.webContents.send("alter-start-stop", "start");
                     if (hasFloating && (process.platform === "darwin")) win.hide();
                 })
             }, 1500)
+        }
     }
 })
 
@@ -1362,9 +1364,11 @@ ipcMain.on('warning-giver-restend', function () {
                 (store.has("personalization-notification.rest-time-end-msg") ?
                     store.get("personalization-notification.rest-time-end-msg") : i18n.__('rest-time-end-msg')), "normal");
         }
-        if (store.get("no-check-time-end")) {
-            noCheckTimeSolution();
-        } else
+        win.webContents.send("alter-start-stop", "stop");
+        if (store.get("no-check-rest-time-end")) {
+            noCheckTimeSolution("rest");
+            setTimeout(() => win.webContents.send("alter-start-stop", "start"), 1000);
+        } else {
             setTimeout(function () {
                 if (process.platform !== "darwin" || (process.platform === "darwin" && workTimeFocused))
                     dialog.showMessageBox(win, {
@@ -1377,6 +1381,7 @@ ipcMain.on('warning-giver-restend', function () {
                             + " " + (hasMultiDisplays ? "\r" + i18n.__('has-multi-displays') : ""),
                     }).then(function () {
                         timeEndDialogDispose("rest");
+                        //win.webContents.send("alter-start-stop", "start");
                     })
                 else dialog.showMessageBox({
                     title: " wnr",
@@ -1388,8 +1393,11 @@ ipcMain.on('warning-giver-restend', function () {
                         + " " + (hasMultiDisplays ? "\r" + i18n.__('has-multi-displays') : ""),
                 }).then(function () {
                     timeEndDialogDispose("rest");
+                    //win.webContents.send("alter-start-stop", "start");
+                    if (hasFloating && (process.platform === "darwin")) win.hide();
                 })
             }, 1000)
+        }
     }
 })
 

settings-renderer.js
@@ -346,12 +346,20 @@ function looseSetting() {
     else store.set("loose-mode", false);
 }
 
-document.getElementById("check-after-time-end-setting").checked = store.get("no-check-time-end") !== true;
+document.getElementById("check-after-work-time-end-setting").checked = store.get("no-check-work-time-end") !== true;
 
-function checkAfterTimeEndSetting() {
-    if (document.getElementById("check-after-time-end-setting").checked === true) store.set("no-check-time-end", false);
-    else store.set("no-check-time-end", true);
+function checkAfterWorkTimeEndSetting() {
+    if (document.getElementById("check-after-work-time-end-setting").checked === true) store.set("no-check-work-time-end", false);
+    else store.set("no-check-work-time-end", true);
 }
+
+document.getElementById("check-after-rest-time-end-setting").checked = store.get("no-check-rest-time-end") !== true;
+
+function checkAfterRestTimeEndSetting() {
+    if (document.getElementById("check-after-rest-time-end-setting").checked === true) store.set("no-check-rest-time-end", false);
+    else store.set("no-check-rest-time-end", true);
+}
+
 
 if (store.get("disable-pausing") === true) {
     document.getElementById("disable-pausing-setting").checked = true;

settings.html
@@ -187,19 +187,38 @@
                 <p class="text-muted small text-right">
                     <script>document.write(i18n.__('notification-settings'));</script>
                 </p>
-                <!-- check after time end setting -->
+                <!-- check after work time end setting -->
                 <div class="row">
                     <div class="col-1 text-center">
-                        <input id="check-after-time-end-setting" type="checkbox" onchange="checkAfterTimeEndSetting()"
+                        <input id="check-after-work-time-end-setting" type="checkbox"
+                               onchange="checkAfterWorkTimeEndSetting()"
                                checked />
                     </div>
                     <div class="col-11 text-left">
                         <label>
-                            <script>document.write(i18n.__('check-after-time-end-setting'));</script>
+                            <script>document.write(i18n.__('check-after-work-time-end-setting'));</script>
                         </label>
                         <br />
                         <span class="text-muted small">
-                            <script>document.write(i18n.__('check-after-time-end-setting-tip'));</script>
+                            <script>document.write(i18n.__('check-after-work-time-end-setting-tip'));</script>
+                        </span>
+                    </div>
+                </div>
+                <br />
+                <!-- check after rest time end setting -->
+                <div class="row">
+                    <div class="col-1 text-center">
+                        <input id="check-after-rest-time-end-setting" type="checkbox"
+                               onchange="checkAfterRestTimeEndSetting()"
+                               checked />
+                    </div>
+                    <div class="col-11 text-left">
+                        <label>
+                            <script>document.write(i18n.__('check-after-rest-time-end-setting'));</script>
+                        </label>
+                        <br />
+                        <span class="text-muted small">
+                            <script>document.write(i18n.__('check-after-rest-time-end-setting-tip'));</script>
                         </span>
                     </div>
                 </div>

timer.html
@@ -481,27 +481,35 @@
                     }
                     player.loop = true;
                     player.play();
-                    if (store.get("no-check-time-end")) {
+                    if ((method === 2 && store.get("no-check-work-time-end")) || (method === 1 && store.get("no-check-rest-time-end"))) {
+                        $("#skipper").css("display", "none");
                         setTimeout(function () {
                             player.loop = false;
+                            $("#skipper").css("display", "inline");
                         }, 3000);
                         $("#skipper-container").css("display", "inline");
                         if (!store.get("islocked")) $("#skipper-space").css("display", "inline");
                         canStop = true;
-                    } else
+                    } else {
+                        setTimeout(function () {
+                            if (isClockWorking) stopper();
+                        }, 1500);
                         setTimeout(function () {
                             player.loop = false;
                         }, 20000);
+                    }
                 }//different sound for different circumstances
                 if (isend === 0) ipc.send('warning-giver-all-task-end');
                 else {
                     if (isend === 1) ipc.send('warning-giver-workend');
                     else ipc.send('warning-giver-restend');
-                    if (!store.get("no-check-time-end")) stopperTimeout = setTimeout(stopper, 500);
+                    if ((method === 2 && store.get("no-check-work-time-end")) || (method === 1 && store.get("no-check-rest-time-end")))
+                        if (!isClockWorking) stopperTimeout = setTimeout(stopper, 500);
                     ipc.once('warning-closed', function () {
                         player.loop = false;
                         player.pause();
-                        stopper();
+
+                        if (!isClockWorking) stopper();
                         $("#skipper-container").css("display", "inline");
                         if (!store.get("islocked")) $("#skipper-space").css("display", "inline");
                         canStop = true;
@@ -608,6 +616,7 @@
                 call('floating-destroy');
                 window.clearInterval(int);
                 $("#time-cnt").css("display", "none");
+                $("#stop-count").css("display", "none");
                 $("#only-one").css("display", "none");
                 $("#fullscreen-experience-tip").css("display", "none");
                 $("#work-rest").text("");

commit d1a038018676a9b9eb871406b4ba778608108aab

fix timing bug caused by 5507a2d

main.js
@@ -1366,7 +1366,6 @@ ipcMain.on('warning-giver-restend', function () {
                 (store.has("personalization-notification.rest-time-end-msg") ?
                     store.get("personalization-notification.rest-time-end-msg") : i18n.__('rest-time-end-msg')), "normal");
         }
-        win.webContents.send("alter-start-stop", "stop");
         if (store.get("no-check-rest-time-end")) {
             noCheckTimeSolution("rest");
             setTimeout(() => win.webContents.send("alter-start-stop", "start"), 1000);