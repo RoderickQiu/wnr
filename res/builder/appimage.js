dd3b2015be7d85887fb9d316c0ae081477188a81
NKID00 <nkid00@pm.me>
2021-09-24 21:59:22 +0800

    build: add AppImage build config

---
+++ res/builder/appimage.js
@@ -0,0 +1,38 @@
+const builder = require('electron-builder')
+const Platform = builder.Platform
+
+require("./env_commit")
+
+const config = {
+    "directories": {
+        "output": "packaged/"
+    },
+    "linux": {
+        "asarUnpack": [
+            "./node_modules/node-notifier/vendor/**"
+        ],
+        "target": [
+            "AppImage"
+        ],
+        "icon": "res/icons/wnrIcon.png",
+        "files": [
+            "**/*",
+            //! to exclude
+            "!res/icons/*Mac*",
+            "!res/icons/*Win*",
+            "!res/icons/*.psd",
+            "!./node_modules/node-notifier/vendor/snoreToast/**"
+        ]
+    }
+}
+
+builder.build({
+    targets: Platform.LINUX.createTarget(),
+    config,
+})
+    .then(m => {
+        console.log(m)
+    })
+    .catch(e => {
+        console.error(e)
+    })
