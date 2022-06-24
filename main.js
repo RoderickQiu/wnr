ff2a5e8886a08d4bcdbd4541592c5d3d8ff06a9a
r-q <scrisqiu@hotmail.com>
2022-06-24 20:26:34 +0800

    update statistics
    separate simple ctdn. and liberal timing mode to be independent roles in statistics page

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ let win = null, settingsWin = null, aboutWin = null, tourWin = null, floatingWin
     tray = null, contextMenu = null, settingsWinContextMenu = null,
     resetAlarm = null, powerSaveBlockerId = null, sleepBlockerId = null,
     isTimerWin = null, isWorkMode = null, isChinese = null, isFocused = true,
+    isOnlyRest = false, isPositiveTiming = false,
     timeLeftTip = null, trayTimeMsg = null, predefinedTasks = null,
     trayH = null, trayMin = null,
     workTimeFocused = false, restTimeFocused = false,
@@ -1265,22 +1266,76 @@ function statisticsWriter() {
     statisticsInitializer();
 
     if (isTimerWin) {
-        if (isWorkMode) {
+        if (isOnlyRest) {
+            let onlyRestTimePeriod = Math.floor((tempDate.getTime() - recorderDate.getTime()) / 60000);
+            recorderDate = tempDate;
+            statistics.set(yearMonDay, {
+                "workTime": statistics.has(yearMonDay) ? statistics.get(yearMonDay).workTime : 0,
+                "restTime": statistics.has(yearMonDay) ? statistics.get(yearMonDay).restTime : 0,
+                "positive": statistics.has(yearMonDay) ? statistics.get(yearMonDay).positive : 0,
+                "onlyRest": statistics.has(yearMonDay) ? statistics.get(yearMonDay).onlyRest + onlyRestTimePeriod : onlyRestTimePeriod,
+                "sum": statistics.has(yearMonDay) ? statistics.get(yearMonDay).sum + onlyRestTimePeriod : onlyRestTimePeriod
+            });
+            statistics.set(yearAndMon, {
+                "workTime": statistics.has(yearAndMon) ? statistics.get(yearAndMon).workTime : 0,
+                "restTime": statistics.has(yearAndMon) ? statistics.get(yearAndMon).restTime : 0,
+                "positive": statistics.has(yearAndMon) ? statistics.get(yearAndMon).positive : 0,
+                "onlyRest": statistics.has(yearAndMon) ? statistics.get(yearAndMon).onlyRest + onlyRestTimePeriod : onlyRestTimePeriod,
+                "sum": statistics.has(yearAndMon) ? statistics.get(yearAndMon).sum + onlyRestTimePeriod : onlyRestTimePeriod
+            });
+            statistics.set(year, {
+                "workTime": statistics.has(year) ? statistics.get(year).workTime : 0,
+                "restTime": statistics.has(year) ? statistics.get(year).restTime : 0,
+                "positive": statistics.has(year) ? statistics.get(year).positive : 0,
+                "onlyRest": statistics.has(year) ? statistics.get(year).onlyRest + onlyRestTimePeriod : onlyRestTimePeriod,
+                "sum": statistics.has(year) ? statistics.get(year).sum + onlyRestTimePeriod : onlyRestTimePeriod,
+            });
+        } else if (isPositiveTiming) {
+            let positiveTimePeriod = Math.floor((tempDate.getTime() - recorderDate.getTime()) / 60000);
+            recorderDate = tempDate;
+            statistics.set(yearMonDay, {
+                "workTime": statistics.has(yearMonDay) ? statistics.get(yearMonDay).workTime : 0,
+                "restTime": statistics.has(yearMonDay) ? statistics.get(yearMonDay).restTime : 0,
+                "positive": statistics.has(yearMonDay) ? statistics.get(yearMonDay).positive + positiveTimePeriod : positiveTimePeriod,
+                "onlyRest": statistics.has(yearMonDay) ? statistics.get(yearMonDay).onlyRest : 0,
+                "sum": statistics.has(yearMonDay) ? statistics.get(yearMonDay).sum + positiveTimePeriod : positiveTimePeriod
+            });
+            statistics.set(yearAndMon, {
+                "workTime": statistics.has(yearAndMon) ? statistics.get(yearAndMon).workTime : 0,
+                "restTime": statistics.has(yearAndMon) ? statistics.get(yearAndMon).restTime : 0,
+                "positive": statistics.has(yearAndMon) ? statistics.get(yearAndMon).positive + positiveTimePeriod : positiveTimePeriod,
+                "onlyRest": statistics.has(yearAndMon) ? statistics.get(yearAndMon).onlyRest : 0,
+                "sum": statistics.has(yearAndMon) ? statistics.get(yearAndMon).sum + positiveTimePeriod : positiveTimePeriod
+            });
+            statistics.set(year, {
+                "workTime": statistics.has(year) ? statistics.get(year).workTime : 0,
+                "restTime": statistics.has(year) ? statistics.get(year).restTime : 0,
+                "positive": statistics.has(year) ? statistics.get(year).positive + positiveTimePeriod : positiveTimePeriod,
+                "onlyRest": statistics.has(year) ? statistics.get(year).onlyRest : 0,
+                "sum": statistics.has(year) ? statistics.get(year).sum + positiveTimePeriod : positiveTimePeriod,
+            });
+        } else if (isWorkMode) {
             let workTimePeriod = Math.floor((tempDate.getTime() - recorderDate.getTime()) / 60000);
             recorderDate = tempDate;
             statistics.set(yearMonDay, {
                 "workTime": statistics.has(yearMonDay) ? statistics.get(yearMonDay).workTime + workTimePeriod : workTimePeriod,
                 "restTime": statistics.has(yearMonDay) ? statistics.get(yearMonDay).restTime : 0,
+                "positive": statistics.has(yearMonDay) ? statistics.get(yearMonDay).positive : 0,
+                "onlyRest": statistics.has(yearMonDay) ? statistics.get(yearMonDay).onlyRest : 0,
                 "sum": statistics.has(yearMonDay) ? statistics.get(yearMonDay).sum + workTimePeriod : workTimePeriod
             });
             statistics.set(yearAndMon, {
                 "workTime": statistics.has(yearAndMon) ? statistics.get(yearAndMon).workTime + workTimePeriod : workTimePeriod,
                 "restTime": statistics.has(yearAndMon) ? statistics.get(yearAndMon).restTime : 0,
+                "positive": statistics.has(yearAndMon) ? statistics.get(yearAndMon).positive : 0,
+                "onlyRest": statistics.has(yearAndMon) ? statistics.get(yearAndMon).onlyRest : 0,
                 "sum": statistics.has(yearAndMon) ? statistics.get(yearAndMon).sum + workTimePeriod : workTimePeriod
             });
             statistics.set(year, {
                 "workTime": statistics.has(year) ? statistics.get(year).workTime + workTimePeriod : workTimePeriod,
                 "restTime": statistics.has(year) ? statistics.get(year).restTime : 0,
+                "positive": statistics.has(year) ? statistics.get(year).positive : 0,
+                "onlyRest": statistics.has(year) ? statistics.get(year).onlyRest : 0,
                 "sum": statistics.has(year) ? statistics.get(year).sum + workTimePeriod : workTimePeriod
             });
         } else {
@@ -1289,16 +1344,22 @@ function statisticsWriter() {
             statistics.set(yearMonDay, {
                 "workTime": statistics.has(yearMonDay) ? statistics.get(yearMonDay).workTime : 0,
                 "restTime": statistics.has(yearMonDay) ? statistics.get(yearMonDay).restTime + restTimePeriod : restTimePeriod,
+                "positive": statistics.has(yearMonDay) ? statistics.get(yearMonDay).positive : 0,
+                "onlyRest": statistics.has(yearMonDay) ? statistics.get(yearMonDay).onlyRest : 0,
                 "sum": statistics.has(yearMonDay) ? statistics.get(yearMonDay).sum + restTimePeriod : restTimePeriod
             });
             statistics.set(yearAndMon, {
                 "workTime": statistics.has(yearAndMon) ? statistics.get(yearAndMon).workTime : 0,
                 "restTime": statistics.has(yearAndMon) ? statistics.get(yearAndMon).restTime + restTimePeriod : restTimePeriod,
+                "positive": statistics.has(yearAndMon) ? statistics.get(yearAndMon).positive : 0,
+                "onlyRest": statistics.has(yearAndMon) ? statistics.get(yearAndMon).onlyRest : 0,
                 "sum": statistics.has(yearAndMon) ? statistics.get(yearAndMon).sum + restTimePeriod : restTimePeriod
             });
             statistics.set(year, {
                 "workTime": statistics.has(year) ? statistics.get(year).workTime : 0,
                 "restTime": statistics.has(year) ? statistics.get(year).restTime + restTimePeriod : restTimePeriod,
+                "positive": statistics.has(year) ? statistics.get(year).positive : 0,
+                "onlyRest": statistics.has(year) ? statistics.get(year).onlyRest : 0,
                 "sum": statistics.has(year) ? statistics.get(year).sum + restTimePeriod : restTimePeriod
             });
         }
@@ -1755,6 +1816,14 @@ ipcMain.on('window-hide', function () {
 
 ipcMain.on('window-minimize', function () {
     if (win != null) win.minimize()
+})
+
+ipcMain.on('enter-only-rest', function () {
+    isOnlyRest = true;
+})
+
+ipcMain.on('enter-positive-timing', function () {
+    isPositiveTiming = true;
 })
 
 function about() {
@@ -2225,6 +2294,9 @@ ipcMain.on("timer-win", function (event, message) {
         if (store.get("tray-time") !== false && process.platform === "darwin")
             tray.setTitle(' ' + i18n.__('not-timing'));
         else tray.setTitle("");
+
+        isOnlyRest = false;
+        isPositiveTiming = false;
     }
 })
 
