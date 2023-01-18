ccf2d61659b002dc8ea35e960a731df76e21cd11
r-q <scrisqiu@hotmail.com>
2023-01-18 20:24:36 +0800

    fix win null edge case

--- main.js
+++ main.js
@@ -241,23 +241,27 @@ function multiScreenSolution(mode) {
     if (app.isReady()) {
         displays = screen.getAllDisplays();
         hasMultiDisplays = (displays.length > 1);
-        let winBounds = win.getBounds();
-        //get the screen that contains the window
-        let distScreen = screen.getDisplayNearestPoint({ x: winBounds.x, y: winBounds.y });
-        for (i in displays) {
-            if (displays[i].id !== distScreen.id) {
-                if (mode === "on") {
-                    addScreenSolution(i, displays[i]);
-                } else {
-                    if (newWindows[i] != null) {
-                        try {
-                            newWindows[i].destroy();
-                        } catch (e) {
-                            console.log(e);
+        try {
+            let winBounds = win.getBounds();
+            //get the screen that contains the window
+            let distScreen = screen.getDisplayNearestPoint({ x: winBounds.x, y: winBounds.y });
+            for (i in displays) {
+                if (displays[i].id !== distScreen.id) {
+                    if (mode === "on") {
+                        addScreenSolution(i, displays[i]);
+                    } else {
+                        if (newWindows[i] != null) {
+                            try {
+                                newWindows[i].destroy();
+                            } catch (e) {
+                                console.log(e);
+                            }
                         }
                     }
                 }
             }
+        } catch (e) {
+            console.log("ERR: multi screen solution failed.");
         }
     }
 }
