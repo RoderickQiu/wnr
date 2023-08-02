affbeed474f0f53f52f8d36db4a25963aa4737a4
r-q <scrisqiu@hotmail.com>
2023-08-02 16:09:41 +0800

    optim custom dialog
    press "enter" and go for simpler dialogs

--- main.js
+++ main.js
@@ -739,33 +739,36 @@ function customDialog(mode, title, msg, executeAfter) {
     }
     if (executeAfter == null) executeAfter = "";
     if (mode === "on" || mode === "select_on" || mode === "update_on") {
-        customDialogWin.webContents.send("dialog-init", {
-            title: title,
-            msg: msg,
-            executeAfter: executeAfter,
-            type: getCustomDialogModeType(mode)
-        });
-        customDialogWin.show();
-        customDialogWin.setAlwaysOnTop(true, "pop-up-menu");
-        customDialogWin.focus();
-        if (mode === "update_on") {
-            customDialogWin.setSize(Math.floor(240 * ratio) * 2, customDialogWin.getSize()[1]);
-        } else {
-            customDialogWin.setSize(Math.floor(240 * ratio), customDialogWin.getSize()[1]);
+        if (customDialogWin != null) {
+            customDialogWin.webContents.send("dialog-init", {
+                title: title,
+                msg: msg,
+                executeAfter: executeAfter,
+                type: getCustomDialogModeType(mode)
+            });
+            customDialogWin.show();
+            customDialogWin.setAlwaysOnTop(true, "pop-up-menu");
+            customDialogWin.focus();
+            if (mode === "update_on") {
+                customDialogWin.setSize(Math.floor(240 * ratio) * 2, customDialogWin.getSize()[1]);
+            } else {
+                customDialogWin.setSize(Math.floor(240 * ratio), customDialogWin.getSize()[1]);
+            }
+            customDialogWin.center();
         }
-        customDialogWin.center();
     } else if (mode === "off") {
-        customDialogWin.hide();
+        if (customDialogWin != null) customDialogWin.hide();
         try {
             eval(executeAfter);
         } catch (e) {
             console.log(e);
         }
     } else if (mode === "cancel") {
-        customDialogWin.hide();
+        if (customDialogWin != null) customDialogWin.hide();
     } else if (mode === "button3_update") {
         shell.openExternal("https://github.com/RoderickQiu/wnr/releases/latest");
-        customDialogWin.hide();
+
+        if (customDialogWin != null) customDialogWin.hide();
     }
 }
 
