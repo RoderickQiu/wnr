6499ad5037c4f6043c2746d154439b69498d0e1b
r-q <scrisqiu@hotmail.com>
2025-11-09 23:50:54 +0800

    fix not check time buggy pop up

--- main.js
+++ main.js
@@ -1711,8 +1711,8 @@ function noCheckTimeSolution(mode) {
         fullScreenProtection = true;
     } else {
         if (store.get("top") !== true) {
-            win.setAlwaysOnTop(false);//cancel unnecessary always-on-top
-            if ((mode === "work" && store.get("when-work-time-end") === 2) || (mode === "resdt" && store.get("when-rest-time-end") !== 2)) {
+            win.setAlwaysOnTop(false); // cancel unnecessary always-on-top
+            if ((mode === "work" && store.get("when-work-time-end") === 2) || (mode === "rest" && store.get("when-rest-time-end") === 2)) {
             } else if (!hasFloating) win.moveTop();
         }
         if (dockHide) app.dock.hide();
